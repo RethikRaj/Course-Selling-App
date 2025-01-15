ec8d13ca4fa3ba846efffe8d28cdb5de77e123b2
Rethik Raj <rethikrajrr@gmail.com>
2025-01-15 12:17:07 +0530

    added middlewares for user and admin auth

--- backend/src/middlewares/adminAuth.js
+++ backend/src/middlewares/adminAuth.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config({path : "../../.env"});
 
-const JWT_SECRET  = process.env.JWT_ADMIN_SECRET;
+const JWT_ADMIN_SECRET  = process.env.JWT_ADMIN_SECRET;
 
 function adminAuth(req,res,next){
     const token = req.headers.authorization;
     try{
-        const decodedInfo = jwt.verify(token,JWT_SECRET);
+        const decodedInfo = jwt.verify(token,JWT_ADMIN_SECRET);
         const id = decodedInfo.id;
         req.adminId = id;
         next();
--- backend/src/middlewares/userAuth.js
+++ backend/src/middlewares/userAuth.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
-require('dotenv').config({path : "../.env"});
+require('dotenv').config({path : "../../.env"});
 
-const JWT_SECRET  = process.env.JWT_USER_SECRET;
+const JWT_USER_SECRET  = process.env.JWT_USER_SECRET;
 
 function userAuth (req,res,next){
     const token = req.headers.authorization;
     try{
-        const decodedInfo = jwt.verify(token,JWT_SECRET);
+        const decodedInfo = jwt.verify(token,JWT_USER_SECRET);
         const id = decodedInfo.id;
         req.userId = id;
         next();
